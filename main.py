e43d7c99ceee992facd6681640e632b9c4fb4d59
Amrit Rai <iamamrit27@gmail.com>
2024-01-07 11:17:40 +0530

    switch to dhanhq

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from fyers_apiv3 import fyersModel
+from dhanhq import dhanhq
 
-client_id = "XC4XXXXM-100"
-access_token = "eyJ0eXXXXXXXX2c5-Y3RgS8wR14g"
+dhan = dhanhq("1101799004","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzA3MTkzODk1LCJ0b2tlbkNvbnN1bWVyVHlwZSI6IlNFTEYiLCJ3ZWJob29rVXJsIjoiIiwiZGhhbkNsaWVudElkIjoiMTEwMTc5OTAwNCJ9.ssSMF0-CPJ2_SQ-ejib-0qqhy-z7bQEXH3Wh6UcH8OLwi99bP0WX31DR97k-rXUyNmF7I9qekjbRgeC1BSoAgg")
 
-# Initialize the FyersModel instance with your client_id, access_token, and enable async mode
-fyers = fyersModel.FyersModel(
-    client_id=client_id, token=access_token, is_async=True, log_path=""
+dhan.place_order(data)
+
+dhan.place_order(
+    tag='',
+    transaction_type=dhan.BUY,
+    exchange_segment=dhan.NSE,
+    product_type=dhan.INTRA,
+    order_type=dhan.MARKET,
+    validity='DAY',
+    security_id='1333',
+    quantity=10,
+    disclosed_quantity=0,
+    price=0,
+    trigger_price=0,
+    after_market_order=False,
+    amo_time='OPEN',
+    bo_profit_value=0,
+    bo_stop_loss_Value=0,
+    drv_expiry_date=None,
+    drv_options_type=None,
+    drv_strike_price=None    
 )
-
-# Make a request to get the funds information
-response = fyers.funds()
-print(response)
